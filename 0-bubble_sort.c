35f76a1df29a24f0ab42751c368257dc96cc12c1
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-14 22:10:45 +0000

    add betty for the file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,26 +3,29 @@
 #include "sort.h"
 
 /**
- * main - Entry point
+ *  bubble_sort - function that sorts
  *
- * Return: Always 0
+ * @array: The array
+ * @size: Number of elements in @array
  */
 
-void bubble_sort(int *array, size_t size){
+void bubble_sort(int *array, size_t size)
+{
 
-    size_t i,j; /**Variables contadoras del ciclo.*/
-    size_t temp=0;
+	size_t i, j; /**Variables contadoras del ciclo.*/
+	size_t temp;
 
-    for (i=1;i<size;i++)
-    {
-        for (j=0; j < size ;j++) /** for(j=0; j < Nelementos-i; j++) es menor y no menor igual*/
-        {
-            if (array[j] > array[j+1])/**Condicion mayor-menor*/
-            {
-                temp=array[j];
-                array[j]=array[j+1];
-                array[j+1]=temp;
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++) /**es menor y no menor igual*/
+		{
+			if (array[j] > array[j + 1])/**Condicion mayor-menor*/
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
