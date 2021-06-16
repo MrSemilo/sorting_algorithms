b3fe336e528788aaeac1546bfac59cfceaec78ca
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-06-16 11:47:28 -0500

    Betty stylle

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,32 +2,33 @@
 
 /**
  * selection_sort - Prints a list of integers
- *
+ * @array: The array
  * @list: The list to be printed
+ * @size: size
  */
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, x;
-	int temp;
+size_t i, j, x;
+int temp;
 
-	if (array == NULL || size < 2)
-		return;
+if (array == NULL || size < 2)
+return;
 
-	for (i = 0; i < (size - 1); i++)
-	{
-		x = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[x])
-				x = j;
-		}
-        if (x != i)
-		{
-			temp = array[x];
-			array[x] = array[i];
-			array[i] = temp;
-			print_array(array, size);
-		}
-    }
+for (i = 0; i < (size - 1); i++)
+{
+x = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[x])
+x = j;
+}
+if (x != i)
+{
+temp = array[x];
+array[x] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
+}
 }
